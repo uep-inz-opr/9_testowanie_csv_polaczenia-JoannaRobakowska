15528d24fdc792dfe75c7646b9a8507adbb1ebaf
Joanna <86994@student.ue.poznan.pl>
2022-06-05 22:01:26 +0200

    podejscie kolejne

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
 
 def wczytaj(self):
-	dane = []
-
+    calls_dict_sum = dict()
 
 with open('phoneCalls.csv', 'r') as fin:
 	reader = csv.reader(fin, delimiter= "'")
@@ -10,14 +9,14 @@ with open('phoneCalls.csv', 'r') as fin:
 
 	for row in reader:
 		from_subsr =int(row[0])
-		if from_subsr == 0:
-			from_subsr += 1
-		else:
-			dane.append(row[0])
+        if from_subsr not in calls_dict_sum:
+          calls_dict_sum[from_subsr] = 0
+        calls_dict_sum[from_subsr] += 1
+    return calls_dict_sum
 
 
 def pobierz_najczesciej_dzwoniacego(self):
-	return max(((int(dane), numbers.count(dane)) for dane in set(dane)), key = lambda x: x[1])		
+    return max(self.data_dict.items(), key= lambda x: x[1])	
 
 if __name__ == "__main__":
 print(wczytaj(input()).pobierz_najczesciej_dzwoniacego())
